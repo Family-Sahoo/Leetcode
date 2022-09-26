b2757b11ef0e68d57138540dbec7fff4ab4acdb8
Kamal Sahoo <kamal.ks5a0@gmail.com>
2022-09-25 18:31:51 -0700

    Adding file CapitalizeFirst.py

--- CapitalizeFirst.py
+++
@@ -1,9 +0,0 @@
-def CapitalizeFirst(arr):
-    if len(arr) == 0:
-        return []
-    if len(arr) == 1:
-        return [arr[0].capitalize()]
-    else:
-        return [arr[0].capitalize()] + (CapitalizeFirst(arr[1:]))
-
-print(CapitalizeFirst(['bus', 'train']))
